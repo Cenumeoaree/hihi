2ecf566c4bc327a9ea8709a9c8836c6e5647a181
Cenumeoaree <83758999+Cenumeoaree@users.noreply.github.com>
2022-03-10 18:42:16 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,25 +1,11 @@
-z = int(input("Introduceti ziua nasterii: "))
+n = int(input("Introduceti un numar natural: "))
 
-l = int(input("Introduceti luna nasterii: "))
+v = []
 
-a = int(input("Introduceti anul nasterii: "))
+while n > 0:
+    a = n % 10
+    if a not in v:
+        v.append(a)
+    n = n // 10
 
-s = 0
-
-s += int(z % 10 + z / 10 % 10)
-
-s += int(l % 10 + l / 10 % 10)
-
-s += int(int(a % 10) + int(a / 10 % 10) + int(a / 100 % 10) + int(a / 1000 % 10))
-
-
-if s < 10:
-    print("Cifra destinului este: ", s)
-else:
-    s += int(int(s % 10) + int(s / 10 % 10) - s)
-
-if s < 10:
-    print("Cifra destinului este: ", s)
-else:
-    s += int(int(s % 10) + int(s / 10 % 10) - s)
-    print("Cifra destinului este: ", s)
+print(v)
